74eaa9fe6ef3632c459fed0c37a517f251ae85df
Owen Synge <osynge@googlemail.com>
2022-04-07 00:09:55 +0200

    add new tests

---
+++ clone_or_test/src/generic.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+
+mod tests {
+    use clone_or::CloneOr;
+    extern crate pretty_assertions;
+    fn config_precedence<T>(cli_cfg: &T, env_cfg: &T, file_cfg: &T) -> T
+    where
+        T: CloneOr,
+    {
+        cli_cfg.clone_or(env_cfg).clone_or(env_cfg)
+    }
+    #[derive(CloneOr, PartialEq)]
+    struct Simple {
+        number: Option<i32>,
+    }
+    #[test]
+    fn test_patterns() {
+        let empty = Simple { number: None };
+        let some_1 = Simple { number: Some(1) };
+        let some_2 = Simple { number: Some(2) };
+        let out = config_precedence(&empty, &some_1, &some_2);
+        assert!(out == some_1);
+        let out = config_precedence(&empty, &some_2, &some_1);
+        assert!(out == some_2);
+    }
+}
--- clone_or_test/src/lib.rs
+++ clone_or_test/src/lib.rs
@@ -1,7 +1,8 @@
 //! This crate provides a convenient concise way to write unit tests for
-//! implementations of [`CloneOr`].
+//! implementations of [clone_or::CloneOr].
 extern crate clone_or;
 #[cfg_attr(test, macro_use)]
 extern crate clone_or_derive;
 mod core;
+mod generic;
 mod nested;
